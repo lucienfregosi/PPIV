b4271ba8fc7d6e69c037d0a34ed330a2dd14a0a7
FREGOSI Lucien (ext INVENTIV-IT) <ELFI03951@COMMUN.AD.SNCF.fr>
2017-06-21 15:59:30 +0200

    Changement du nom de fichier de sortie pour qu'il soit indexé par heure

--- src/main/scala/com/sncf/fab/ppiv/pipelineData/TraitementTgd.scala
+++ src/main/scala/com/sncf/fab/ppiv/pipelineData/TraitementTgd.scala
@@ -11,9 +11,9 @@ class TraitementTgd extends SourcePipeline {
 
   override def getSource() = LANDING_WORK_HDFS  + Conversion.getYearMonthDay(Conversion.nowToDateTime()) + "/TGD-" + Conversion.getYearMonthDay(Conversion.nowToDateTime()) + "_" + Conversion.getHour(Conversion.nowToDateTime()) + ".csv"
 
-  override def getOutputGoldPath() = GOLD_HDFS + Conversion.getYearMonthDay(new DateTime()) + "_TGD.csv"
+  override def getOutputGoldPath() = GOLD_HDFS + Conversion.getYearMonthDay(new DateTime()) + "_TGD_" + Conversion.getYearMonthDay(Conversion.nowToDateTime()) + ".csv"
 
-  override def getOutputRefineryPath() = REFINERY_HDFS + Conversion.getYearMonthDay(new DateTime()) + "_TGD.csv"
+  override def getOutputRefineryPath() = REFINERY_HDFS + Conversion.getYearMonthDay(new DateTime()) + "_TGD_" + Conversion.getYearMonthDay(Conversion.nowToDateTime()) + ".csv"
 
   override def Depart(): Boolean = true
 
