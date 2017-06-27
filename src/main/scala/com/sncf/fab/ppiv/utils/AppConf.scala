62c49d9a5e4bf9c19a01e2adb9618361c5863d16
FREGOSI Lucien (ext INVENTIV-IT) <ELFI03951@COMMUN.AD.SNCF.fr>
2017-06-27 11:37:11 +0200

    ajout du chemin pour le logger

--- src/main/scala/com/sncf/fab/ppiv/utils/AppConf.scala
+++ src/main/scala/com/sncf/fab/ppiv/utils/AppConf.scala
@@ -17,7 +17,10 @@ object AppConf extends Serializable{
   val LANDING_WORK = conf.getString("landing_work")
   val REF_GARES    = conf.getString("ref_gares")
 
-  // Valeut TGA et TGD
+  // Chemin ou enregistrer les logs
+  val LOG_PATH     = conf.getString("log")
+
+  // Valeur TGA et TGD
   val TGA          = "TGA.csv"
   val TGD          = "TGD.csv"
 
