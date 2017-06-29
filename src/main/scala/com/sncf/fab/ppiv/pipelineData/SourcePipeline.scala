a4cf2ca34360b4a36e285a169377a5152d349d18
Gafsi Samir <ext.samir.gafsi@sncf.fr>
2017-06-29 19:39:03 +0200

    Field Validation

--- src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
+++ src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
@@ -165,9 +165,10 @@ trait SourcePipeline extends Serializable {
       .filter(_.picto.toInt >=0)
       .filter(_.attribut_voie matches "I||\\s")
       .filter(_.voie matches "^(?:[0-9]|[A-Z])$")
-      /*.filter(_.heure <= currentTimestamp)
-      .filter(_.etat matches "(IND||SUP||ARR||\\s)")
-      .filter(_.retard matches  "(^[0-9]{2}|[0-9]{4}|\\s$)")*/
+      .filter(_.heure <= currentTimestamp)
+      .filter(_.etat matches "^(?:IND|SUP|ARR|\\s)$)")
+      //.filter(_.etat matches "(IND||SUP||ARR||\\s)")
+     // .filter(_.retard matches  "(^[0-9]{2}|[0-9]{4}|\\s$)")*/
 
     dsTgaTgdValidatedFields.show()
     dsTgaTgdValidatedFields
