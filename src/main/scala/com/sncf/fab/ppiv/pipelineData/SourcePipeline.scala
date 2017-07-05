c8687e212a66b5278fd46b9093d8715a20428449
Gafsi Samir <ext.samir.gafsi@sncf.fr>
2017-07-05 12:51:45 +0200

    Validation des champs

--- src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
+++ src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
@@ -190,24 +190,25 @@ trait SourcePipeline extends Serializable {
     // Valid
     //dsTgaTgd.show()
     val dsTgaTgdValidatedFields = dsTgaTgd
-      .filter(_.gare matches("^[A-Z]{3}$"))
+      .filter(_.gare matches "^[A-Z]{3}$" )
       .filter(_.maj <= currentTimestamp)
-      .filter(_.train matches  "(^[0-2]{0,1}[0-9]$)")
-      .filter(_.ordes matches "(^[A-Z|\\s]*$)") // Il en enlève 2000
-      .filter(_.`type` matches "^([A-Z]+$)") // Il en enlève 100
-      .filter(x => ((x.attribut_voie matches "I") && (x.voie matches "^(?:[0-9]|[A-Z]|$)$" )) ||((x.attribut_voie matches "\\s||$") && (x.voie matches "^(?:[0-9]|[A-Z])$" )))
-      .filter(_.etat matches "^(?:(IND)|(SUP)|(ARR)|$|(\\s))$")
-      .filter(_.retard matches  "^(([0-9]{4})|([0-9]{2})|$|\\s)$")
+      .filter(_.train matches  "^[0-2]{0,1}[0-9]$")
+     // .filter(_.ordes matches "(^[A-Z|\\s]*$)")
+      //.filter(_.`type` matches "^([A-Z]+$)") // Il en enlève 100
+     // .filter(x => ((x.attribut_voie matches "I") && (x.voie matches "^(?:[0-9]|[A-Z]|$)$" )) ||((x.attribut_voie matches "\\s||$") && (x.voie matches "^(?:[0-9]|[A-Z])$" )))
+     // .filter(_.etat matches "^(?:(IND)|(SUP)|(ARR)|$|(\\s))$")
+     // .filter(_.retard matches  "^(([0-9]{4})|([0-9]{2})|$|\\s)$")
 
     // Rejected
    val dsTgaTgdRejectedFields = dsTgaTgd.filter(x => (x.gare matches("^(?!([A-Z]{3}))$")) || (x.maj > currentTimestamp)
      ||  (x.train matches  "^(?!([0-2]{0,1}[0-9]))$")
      ||(x.train matches  "^(?!([0-2]{0,1}[0-9]))$")
-     ||  (x.ordes matches "^(?!([A-Z|\\s]*))$")
-     ||  (x.`type` matches "^(?!([A-Z]+))$")
-     ||((x.attribut_voie matches "(?!(^I$))") || (x.voie matches "^(?!(?:[0-9]|[A-Z]|$))$" )) &&((x.attribut_voie matches "(?!(\\s||$))") || (x.voie matches "^(?!(?:[0-9]|[A-Z]))$" ))
-        || (x.etat matches "^(?!(?:(IND)|(SUP)|(ARR)|$|\\s))$")
-     || (x.retard matches  "^(?!(?:[0-9]{2}|[0-9]{4}|$|\\s))$"))
+     //||  (x.ordes matches "^(?!([A-Z|\\s]*))$")
+    // ||  (x.`type` matches "^(?!([A-Z]+))$")
+     //||((x.attribut_voie matches "(?!(^I$))") || (x.voie matches "^(?!(?:[0-9]|[A-Z]|$))$" )) &&((x.attribut_voie matches "(?!(\\s||$))") || (x.voie matches "^(?!(?:[0-9]|[A-Z]))$" ))
+        //|| (x.etat matches "^(?!(?:(IND)|(SUP)|(ARR)|$|\\s))$")
+     //|| (x.retard matches  "^(?!(?:[0-9]{2}|[0-9]{4}|$|\\s))$")
+   )
 
 
 
