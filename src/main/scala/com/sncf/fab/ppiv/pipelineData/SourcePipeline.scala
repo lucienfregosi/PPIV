8d63453288097e362c833eff9f9200ee71d8610a
FREGOSI Lucien (ext INVENTIV-IT) <ELFI03951@COMMUN.AD.SNCF.fr>
2017-07-05 17:08:52 +0200

    on le passe en string

--- src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
+++ src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
@@ -281,7 +281,7 @@ trait SourcePipeline extends Serializable {
 
     rddGroupByCycleOver.toDF().show()*/
 
-    val dfGroupByCycleOver = hiveDataframe.drop("cycle_id2").select($"cycle_id", array($"gare",$"maj",$"train",$"ordes",$"num",$"type",$"picto",$"attribut_voie",$"voie",$"heure",$"etat",$"retard").toString() as "event")
+    val dfGroupByCycleOver = hiveDataframe.drop("cycle_id2").select($"cycle_id", concat($"gare",$"maj",$"train",$"ordes",$"num",$"type",$"picto",$"attribut_voie",$"voie",$"heure",$"etat",$"retard"))
       .agg(
         collect_list($"event") as "event"
       )
