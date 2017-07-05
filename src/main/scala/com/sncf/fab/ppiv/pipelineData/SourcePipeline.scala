b02bb9375412f24c6d4a6b99ee7db341abdeae45
Gafsi Samir <ext.samir.gafsi@sncf.fr>
2017-07-05 14:07:45 +0200

    Modif Validation champs

--- src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
+++ src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
@@ -202,7 +202,7 @@ trait SourcePipeline extends Serializable {
     // Rejected
    val dsTgaTgdRejectedFields = dsTgaTgd.filter(x => (x.gare matches("^(?!([A-Z]{3}))$")) || (x.maj > currentTimestamp)
      ||  (x.train matches  "^(?!([0-2]{0,1}[0-9]))$")
-     ||(x.train matches  "^(?!([0-2]{0,1}[0-9]))$")
+
 
      //||  (x.`type` matches "^(?!([A-Z]+))$")
      //||((x.attribut_voie matches "(?!(^I$))") || (x.voie matches "^(?!(?:[0-9]|[A-Z]|$))$" )) &&((x.attribut_voie matches "(?!(\\s||$))") || (x.voie matches "^(?!(?:[0-9]|[A-Z]))$" ))
