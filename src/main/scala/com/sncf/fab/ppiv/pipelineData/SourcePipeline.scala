148b36ec4ad3ad68b6d9e499c89fd451ec7ff5c7
FREGOSI Lucien (ext INVENTIV-IT) <ELFI03951@COMMUN.AD.SNCF.fr>
2017-07-05 13:57:27 +0200

    test pour collect_list

--- src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
+++ src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
@@ -240,9 +240,10 @@ trait SourcePipeline extends Serializable {
   }
 
   def getEventCycleId(dsTgaTgdCyclesOver : Dataset[TgaTgdCycle], sqlContext : SQLContext, sc : SparkContext): DataFrame = {
-    import sqlContext.implicits._
 
-    val hiveContext: SQLContext = new HiveContext(sc)
+
+    val sqlContext: SQLContext = new HiveContext(sc)
+    import sqlContext.implicits._
 
     // A partir de la liste des cycles finis, reconstitution d'un DS de la forme cycleId| Seq(gare, maj, ...)
     val tgaTgdInputAllDay = loadTgaTgd(sqlContext).toDF().withColumn("cycle_id2",concat(col("gare"),lit(Panneau()),col("num"), col("heure")))
