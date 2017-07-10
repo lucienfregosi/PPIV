cfd4af71faf4f663d526dc08e4ab4955eef50412
FREGOSI Lucien (ext INVENTIV-IT) <ELFI03951@COMMUN.AD.SNCF.fr>
2017-07-10 11:54:30 +0200

    go back

--- src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
+++ src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
@@ -114,11 +114,13 @@ trait SourcePipeline extends Serializable {
       // Problème sur le split si il y a des espaces
       val stringSplit = stringLine.split(" ").toSeq
 
-      val objectSeq = stringSplit.map( x => x.split(",").toSeq)
+
+
+      //val objectSeq = stringSplit.map( x => x.split(",").toSeq)
 
       //val seqTgaTgdInput = objectSeq.map(x => TgaTgdInput(x(0), x(1).toLong,x(2),x(3),x(4),x(5),x(6),x(7),x(8),x(9).toLong,x(10),x(11)))
       //seqTgaTgdInput
-      objectSeq
+      stringSplit
     }
 
     t.take(5).foreach(println)
