bdbee3a0c45742c369034fdffbf9ba33c8399ee2
FREGOSI Lucien (ext INVENTIV-IT) <ELFI03951@COMMUN.AD.SNCF.fr>
2017-07-11 14:33:14 +0200

    test3

--- src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
+++ src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
@@ -110,13 +110,15 @@ trait SourcePipeline extends Serializable {
       //val cycleId = x.getString(0)
       //val eventTgaTgd = x.getAs[Row(1)
 
+
+
       val seqTgaTgd = x.toSeq.map(x => {
         // Boucle sur les évènements pour pouvoir construire des Seq[TgaTgdInput)
         val split = x.toString.split(",")
         TgaTgdInput(split(0), split(1).toLong, split(2), split(3), split(4), split(5), split(6), split(7), split(8), split(9).toLong, split(10), split(11))
       })
 
-      seqTgaTgd
+      x.toSeq.toArray.length
 
 
       // 6) Validation des cycles
