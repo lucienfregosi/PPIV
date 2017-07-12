7c3aa05920a6ab288d0a37afc557872bd144f286
FREGOSI Lucien (ext INVENTIV-IT) <ELFI03951@COMMUN.AD.SNCF.fr>
2017-07-12 12:27:58 +0200

    test hive 2

--- src/main/scala/com/sncf/fab/ppiv/persistence/PersistHive.scala
+++ src/main/scala/com/sncf/fab/ppiv/persistence/PersistHive.scala
@@ -36,7 +36,7 @@ object PersistHive extends Serializable {
     dfHive.registerTempTable("dataToSaveHive")
 
     // Chargement des données de HDFS dans Hive
-    hiveContext.sql("CREATE EXTERNAL TABLE IF NOT EXISTS ppiv_ref.iv_tgatgd3 (nom_de_la_gare String, agence String," +
+    /*hiveContext.sql("CREATE EXTERNAL TABLE IF NOT EXISTS ppiv_ref.iv_tgatgd3 (nom_de_la_gare String, agence String," +
       " segmentation  String, uic String, x String, y String, id_train String, num_train String, type String," +
       " origine_destination String, type_panneau String, dateheure2 String,creneau_horaire String, jour_depart_arrivee INT," +
       " jour_depart_arrivee1 String, affichage_duree1 String, affichage_duree1_minutes String, delai_affichage_voie_sans_retard String, " +
@@ -46,7 +46,7 @@ object PersistHive extends Serializable {
       " delai_affichage_etat_train_avant_depart_arrivee_min String, quai_devoiement String, quai_devoiement2 String, quai_devoiement3 String, quai_devoiement4 String," +
       " dernier_quai_affiche String, devoiement INT, devoiement_affiche INT, devoiement_non_affiche INT, type_devoiement String, type_devoiement2 String," +
       " type_devoiement3 String, type_devoiement4 String, carac_devoiement String, date_process String, objectif_h INT)" +
-      " row format delimited fields terminated by ','")
+      " row format delimited fields terminated by ','")*/
 
 
     // Load data to HDFS
@@ -56,7 +56,7 @@ object PersistHive extends Serializable {
     //println("log pour être sur que ca a bien marché")
     //hiveContext.sql("FROM ppiv_ref.iv_tgatgd3 SELECT * LIMIT 10").collect().foreach(println)
 
-    dfHive.write.mode("append").saveAsTable("ppiv_ref.iv_tgatgd3")
+    hiveContext.sql("CREATE EXTERNAL TABLE IF NOT EXISTS ppiv_ref.iv_tgatgd4 as select * from dataToSaveHive")
 
   }
 
