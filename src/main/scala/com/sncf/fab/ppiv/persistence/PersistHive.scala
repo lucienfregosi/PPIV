8119a8bf79edc33bb3eb27db38e44e5353eb6c2f
FREGOSI Lucien (ext INVENTIV-IT) <ELFI03951@COMMUN.AD.SNCF.fr>
2017-07-12 12:42:46 +0200

    amélioration pour hive2

--- src/main/scala/com/sncf/fab/ppiv/persistence/PersistHive.scala
+++ src/main/scala/com/sncf/fab/ppiv/persistence/PersistHive.scala
@@ -46,7 +46,7 @@ object PersistHive extends Serializable {
       " delai_affichage_etat_train_avant_depart_arrivee_min String, quai_devoiement String, quai_devoiement2 String, quai_devoiement3 String, quai_devoiement4 String," +
       " dernier_quai_affiche String, devoiement INT, devoiement_affiche INT, devoiement_non_affiche INT, type_devoiement String, type_devoiement2 String," +
       " type_devoiement3 String, type_devoiement4 String, carac_devoiement String, date_process String, objectif_h INT)" +
-      " row format delimited fields terminated by ','")*/
+      " row format delimited fields terminated by ','")
 
 
     // Load data to HDFS
@@ -56,7 +56,7 @@ object PersistHive extends Serializable {
     //println("log pour être sur que ca a bien marché")
     //hiveContext.sql("FROM ppiv_ref.iv_tgatgd3 SELECT * LIMIT 10").collect().foreach(println)
 
-    hiveContext.sql("INSERT INTO TABLE ppiv_ref.iv_tgatgd4 select * from dataToSaveHive")
+    hiveContext.sql("INSERT INTO TABLE ppiv_ref.iv_tgatgd5 select * from dataToSaveHive")
 
   }
 
