94fd5413250e9a24239b2821e396f051b9445012
FREGOSI Lucien (ext INVENTIV-IT) <ELFI03951@COMMUN.AD.SNCF.fr>
2017-07-13 12:24:59 +0200

    enlever le system.exit

--- src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
+++ src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
@@ -170,10 +170,7 @@ trait SourcePipeline extends Serializable {
 
     // 13) Jointure avec le référentiel
     val dataTgaTgdWithReferentiel = Postprocess.joinReferentiel(cycleValidated, dataRefGares, sqlContext)
-
-    dataTgaTgdWithReferentiel.printSchema()
-
-    System.exit(0)
+    
 
     // 14) Inscription dans la classe finale TgaTgdOutput avec conversion et formatage
     val dataTgaTgdOutput = Postprocess.formatTgaTgdOuput(dataTgaTgdWithReferentiel, sqlContext, Panneau())
---
+++ src/main/scala/com/sncf/fab/ppiv/pipelineData/libPipeline/BusinessConversion.scala
@@ -0,0 +1,8 @@
+package com.sncf.fab.ppiv.pipelineData.libPipeline
+
+/**
+  * Created by ELFI03951 on 13/07/2017.
+  */
+object BusinessConversion {
+
+}
