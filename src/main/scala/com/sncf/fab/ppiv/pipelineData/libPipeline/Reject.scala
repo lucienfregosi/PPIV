0b0af2a52bfc0f741783f009762f5241d24214c3
FREGOSI Lucien (ext INVENTIV-IT) <ELFI03951@COMMUN.AD.SNCF.fr>
2017-08-21 13:35:48 +0200

    fix bug enregistrement en double

--- src/main/scala/com/sncf/fab/ppiv/pipelineData/libPipeline/Reject.scala
+++ src/main/scala/com/sncf/fab/ppiv/pipelineData/libPipeline/Reject.scala
@@ -11,17 +11,17 @@ import com.sncf.fab.ppiv.utils.AppConf._
   * Created by ELFI03951 on 12/07/2017.
   */
 object Reject {
-  def saveFieldRejected(dsFieldRejected: Dataset[TgaTgdInput], sparkContext: SparkContext, timeToProcess: DateTime): Unit ={
+  def saveFieldRejected(dsFieldRejected: Dataset[TgaTgdInput], sparkContext: SparkContext, timeToProcess: DateTime, panneau: String): Unit ={
 
     // Sauvegarde des rejets de champs dans refinery
 
-    dsFieldRejected.toDF().write.format("com.databricks.spark.csv").option("header", "true").save(REJECTED_FIELD + "_" + timeToProcess.getMillis + ".csv")
+    dsFieldRejected.toDF().write.format("com.databricks.spark.csv").option("header", "true").save(REJECTED_FIELD + "/" + panneau + "_"  + timeToProcess.getMillis + ".csv")
   }
 
-  def saveCycleRejected(dsFieldRejected: Dataset[TgaTgdIntermediate], sparkContext: SparkContext,timeToProcess: DateTime): Unit ={
+  def saveCycleRejected(dsFieldRejected: Dataset[TgaTgdIntermediate], sparkContext: SparkContext,timeToProcess: DateTime, panneau: String): Unit ={
 
     // Sauvegarde des rejets de cycles dans refinery
-    dsFieldRejected.toDF().write.format("com.databricks.spark.csv").option("header", "true").save(REJECTED_CYCLE + "_" + timeToProcess.getMillis + ".csv")
+    dsFieldRejected.toDF().write.format("com.databricks.spark.csv").option("header", "true").save(REJECTED_CYCLE + "/" + panneau + "_" + timeToProcess.getMillis + ".csv")
   }
 
 }
