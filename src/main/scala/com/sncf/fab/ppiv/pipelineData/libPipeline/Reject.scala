32be62a7603bcd39fc4dc53dcfda290a28c4a513
FREGOSI Lucien (ext INVENTIV-IT) <ELFI03951@COMMUN.AD.SNCF.fr>
2017-08-21 13:16:36 +0200

    bugfix

--- src/main/scala/com/sncf/fab/ppiv/pipelineData/libPipeline/Reject.scala
+++ src/main/scala/com/sncf/fab/ppiv/pipelineData/libPipeline/Reject.scala
@@ -13,13 +13,13 @@ object Reject {
   def saveFieldRejected(dsFieldRejected: Dataset[TgaTgdInput], sparkContext: SparkContext, timeToProcess: DateTime): Unit ={
 
     // Sauvegarde des rejets de champs dans refinery
-    dsFieldRejected.toDF().write.mode(SaveMode.Append).format("com.databricks.spark.csv").option("header", "true").save(REJECTED_FIELD + "_" + timeToProcess.getMillis + ".csv")
+    dsFieldRejected.toDF().write.format("com.databricks.spark.csv").option("header", "true").save(REJECTED_FIELD + "_" + timeToProcess.getMillis + ".csv")
   }
 
   def saveCycleRejected(dsFieldRejected: Dataset[TgaTgdIntermediate], sparkContext: SparkContext,timeToProcess: DateTime): Unit ={
 
     // Sauvegarde des rejets de cycles dans refinery
-    dsFieldRejected.toDF().coalesce(1).write.mode(SaveMode.Append).format("com.databricks.spark.csv").option("header", "true").save(REJECTED_CYCLE + "_" + timeToProcess.getMillis + ".csv")
+    dsFieldRejected.toDF().write.format("com.databricks.spark.csv").option("header", "true").save(REJECTED_CYCLE + "_" + timeToProcess.getMillis + ".csv")
   }
 
 }
