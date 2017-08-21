dee7685e587cd189b048d7568b99b3d1c3e181a2
FREGOSI Lucien (ext INVENTIV-IT) <ELFI03951@COMMUN.AD.SNCF.fr>
2017-08-21 13:21:55 +0200

    bugfix2

--- src/main/scala/com/sncf/fab/ppiv/pipelineData/libPipeline/Reject.scala
+++ src/main/scala/com/sncf/fab/ppiv/pipelineData/libPipeline/Reject.scala
@@ -5,6 +5,7 @@ import com.sncf.fab.ppiv.persistence.Persist
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.Dataset
 import org.joda.time.DateTime
+import com.sncf.fab.ppiv.utils.AppConf._
 
 /**
   * Created by ELFI03951 on 12/07/2017.
@@ -13,6 +14,7 @@ object Reject {
   def saveFieldRejected(dsFieldRejected: Dataset[TgaTgdInput], sparkContext: SparkContext, timeToProcess: DateTime): Unit ={
 
     // Sauvegarde des rejets de champs dans refinery
+
     dsFieldRejected.toDF().write.format("com.databricks.spark.csv").option("header", "true").save(REJECTED_FIELD + "_" + timeToProcess.getMillis + ".csv")
   }
 
