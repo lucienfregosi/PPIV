407d195efeac2447d1961e5c8d31f1026cc5f5f5
FREGOSI Lucien (ext INVENTIV-IT) <ELFI03951@COMMUN.AD.SNCF.fr>
2017-09-19 17:50:12 +0200

    remplissage pour comparaison4

--- src/main/scala/com/sncf/fab/ppiv/spark/batch/TraitementPPIVDriver.scala
+++ src/main/scala/com/sncf/fab/ppiv/spark/batch/TraitementPPIVDriver.scala
@@ -82,7 +82,7 @@ object TraitementPPIVDriver extends Serializable {
 
     val dfHive = hiveContext.createDataFrame(data.rdd, data.schema)
     dfHive.registerTempTable("dataToSaveToHive")
-    hiveContext.sql("CREATE TABLE IF NOT EXISTS ppiv_ref.iv_tgatgdinputrecette3 as select * from dataToSaveToHive")
+    hiveContext.sql("CREATE TABLE ppiv_ref.iv_tgatgdinputrecette3 as select * from dataToSaveToHive")
     System.exit(0)
 
     LOGGER.warn("Démarrage de l'application PPIV")
