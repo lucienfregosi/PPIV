b9f029e3656fc727eb216af910582010a49df412
FREGOSI Lucien (ext INVENTIV-IT) <ELFI03951@COMMUN.AD.SNCF.fr>
2017-09-19 14:58:08 +0200

    remplissage de TOUS les inputs

--- src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
+++ src/main/scala/com/sncf/fab/ppiv/pipelineData/SourcePipeline.scala
@@ -92,7 +92,7 @@ trait SourcePipeline extends Serializable {
 
       val dataTgaTgd                = LoadData.loadTgaTgd(sqlContext, pathFileToLoad,debutPeriode, reprise_flag : Boolean)
       val dataRefGares              = LoadData.loadReferentiel(sqlContext, debutPeriode)
-      
+
 
       LOGGER.warn("Chargement des fichiers OK. Taille du fichier d'entrée en Byte" +  SizeEstimator.estimate(dataTgaTgd.rdd))
 
